b3fa92e04880f214aea217e37906aeb9bd0cff2b
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-08-12 18:10:49 +0500

    updated hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,49 @@
-// Header.jsx
-import React from 'react';
+// // Header.jsx
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './Header.css';
+// import logo from '../assets/logo.png';
+
+
+// const Header = () => {
+//   return (
+//     <header className="header">
+//       <div className="logo">
+//       <img src={logo} alt="Logo" />
+//       </div>
+//       <nav className="nav">
+//         <Link to="/" className="nav-link">Home</Link>
+//         <Link to="/login" className="nav-link">Login</Link>
+//         <Link to="/signup" className="nav-link">Signup</Link>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../assets/logo.png';
 
-
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">
-      <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
-      <nav className="nav">
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/login" className="nav-link">Login</Link>
         <Link to="/signup" className="nav-link">Signup</Link>
